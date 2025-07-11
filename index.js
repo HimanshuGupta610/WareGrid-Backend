1296bbdcb6e143acda09b6e4cc11ffed375fa8d2
Himanshu Gupta <70843701+HimanshuGupta610@users.noreply.github.com>
2025-07-12 00:06:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -557,7 +557,8 @@ cron.schedule('0 0 * * *', runDailyBackup);
 
 
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
 
